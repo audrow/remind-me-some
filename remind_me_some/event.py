b99911d7b663824fd2ab57294c42acb1f203d44d
Audrow Nash <audrow.nash@gmail.com>
2020-09-17 22:30:48 -0700

    Add docs (#9)
    * Make docstrings in action and schedule manager more expressive
    
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>
    
    * Add docs folder
    
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>
    
    * Make docstrings have function arguments and returns
    
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>
    
    * Change README to be RST
    
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>
    
    * Add API and index for docs
    
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>
    
    * Add requirements.txt
    
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>
    
    * Add return docstring for is_exclude_date
    
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>
    
    * Fix flake8 tests
    
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>

--- remind_me_some/event.py
+++ remind_me_some/event.py
@@ -19,7 +19,27 @@ class Event:
             is_ready_fn: Optional[Callable[[], bool]] = None,
             is_completed_fn: Optional[Callable[[], bool]] = None,
     ) -> None:
-        """Initialize an event."""
+        """Initialize an event.
+
+        :param name:
+            The name of the event.
+        :param priority:
+            The priority of the event (for determining its relative
+            importance).
+        :param interest_rate:
+            The rate that the priority of the event grows each step it
+            is pushed back past its original due date.
+        :param callback:
+            A function to be called when the event is run.
+        :param is_ready_fn:
+            A function to determine if the event is ready. If nothing is
+            supplied, this will default to be true if the event has not
+            been completed.
+        :param is_completed_fn:
+            A function to determine if the event has been completed. If
+            nothing is supplied, this will default to be true if the
+            callback has been called at least once.
+        """
         if priority <= 0:
             raise ValueError("Priority must be a positive number")
         if interest_rate < 0:
@@ -46,22 +66,40 @@ class Event:
         return self.name == other.name
 
     def push_forward(self, steps: int = 1) -> None:
-        """Increase the priority of event by applying interest."""
+        """Increase the priority of event by applying interest.
+
+        :param steps:
+            The number of times to apply the interest rate to the event's
+            priority.
+        """
         logger.debug(f"Push forward '{self.name}' by {steps} steps")
         if steps < 1:
             raise ValueError('Must push forward a positive number of steps')
         self.priority *= (1 + self._interest_rate) ** steps
 
     def is_due(self) -> bool:
-        """Check if the event is due."""
+        """Check if the event is due.
+
+        :return:
+            True if the completion function doesn't return true;
+            false otherwise.
+        """
         return not self.is_completed()
 
     def is_called(self) -> bool:
-        """Check if the event has been called."""
+        """Check if the event has been called.
+
+        :return:
+            True if the callback has been called at least once;
+            false otherwise.
+        """
         return self._callback_count > 0
 
     def callback(self) -> Any:
-        """Call the event's callback."""
+        """Call the event's callback.
+
+        :return: Whatever the callback returns.
+        """
         logger.debug(f"Call callback for '{self.name}'")
         if self.is_ready():
             self._callback_count += 1
@@ -72,17 +110,28 @@ class Event:
                 f"Callback '{self.name}' called before it's ready")
 
     def is_ready(self) -> bool:
-        """Check if an event is ready."""
+        """Check if an event is ready.
+
+        :return:
+            True if the ready function returns true and the event
+            has not been completed; false otherwise.
+        """
         return (
             self._try_run_args(self._is_ready_fn)
             and not self.is_completed()
         )
 
     def is_completed(self) -> bool:
-        """Check if an event has been completed."""
+        """Check if an event has been completed.
+
+        :return:
+            True if the is completed function returns true; false
+            otherwise.
+        """
         return self._try_run_args(self._is_completed_fn)
 
     def _try_run_args(self, fn) -> Any:
+        """Try calling a function with and without the self arguement."""
         try:
             return fn()
         except TypeError:
